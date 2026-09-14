458c67de048fd6443e2c200dfcbdda5309e123e5
Cristian Gonzalez <cristian.glez.m@gmail.com>
2026-09-14 15:21:23 +0200

    fix(ci): fix npm publish pipeline
    * updated version to 1.2.1
    * updated node version in *.yml from 22 to 24
    * added step to update to npm@latest in *.yml

--- .devops/publish.sh
+++ .devops/publish.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-set -e
+set -euo pipefail
 
-NPM_TOKEN=$1
-GH_TOKEN=$2
+GH_TOKEN=$1
 
 echo "@cristianglezm:registry=https://registry.npmjs.org" > .npmrc
-echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" >> .npmrc
-npm publish -w @cristianglezm/live-commentary-widget --access public
+npm publish -w @cristianglezm/live-commentary-widget --access public --provenance
 sleep 10s
 echo "@cristianglezm:registry=https://npm.pkg.github.com" > .npmrc
 echo "//npm.pkg.github.com/:_authToken=\${GH_TOKEN}" >> .npmrc
-npm publish -w @cristianglezm/live-commentary-widget --access public
+npm publish -w @cristianglezm/live-commentary-widget --access public --provenance
 sleep 5s
 rm .npmrc
